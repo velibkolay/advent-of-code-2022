91d0c36e8db9b13733cd6885697592714fa00dd9
velibkolay <veliberkaykolay@gmail.com>
2022-12-18 17:32:23 +0100

    day 02 part 2

--- days/day02.py
+++ days/day02.py
@@ -1,10 +1,16 @@
-from enum import Enum
+from enum import IntEnum
 
 
-class Move(Enum):
+class Move(IntEnum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
+
+
+class Result(IntEnum):
+    WIN = 6
+    DRAW = 3
+    LOSE = 0
 
 
 def enum_match(letter):
@@ -16,7 +22,16 @@ def enum_match(letter):
         return Move.SCISSORS
 
 
-def game_point(my_move, opponents_move):
+def result_match(letter):
+    if letter == "Z":
+        return Result.WIN
+    elif letter == "Y":
+        return Result.DRAW
+    else:
+        return Result.LOSE
+
+
+def game_point_move(my_move: Move, opponents_move: Move):
     if my_move == Move.ROCK:
         if opponents_move == Move.PAPER:
             return 0 + 1
@@ -42,14 +57,41 @@ def game_point(my_move, opponents_move):
         return 0
 
 
+def game_point_result(opponents_move: Move, result: Result):
+    if opponents_move == Move.ROCK:
+        if result == Result.WIN:
+            return Move.PAPER + Result.WIN
+        elif result == Result.DRAW:
+            return Move.ROCK + Result.DRAW
+        else:
+            return Move.SCISSORS + Result.LOSE
+    elif opponents_move == Move.PAPER:
+        if result == Result.WIN:
+            return Move.SCISSORS + Result.WIN
+        elif result == Result.DRAW:
+            return Move.PAPER + Result.DRAW
+        else:
+            return Move.ROCK + Result.LOSE
+    else:
+        if result == Result.WIN:
+            return Move.ROCK + Result.WIN
+        elif result == Result.DRAW:
+            return Move.SCISSORS + Result.DRAW
+        else:
+            return Move.PAPER + Result.LOSE
+
+
 input_day02 = open("../inputs/input_day02.txt").read()
 
 games = input_day02.split("\n")
-total_score = 0
+total_score_part1 = 0
+total_score_part2 = 0
 
 for game in games:
     opponent = enum_match(game.split()[0])
     me = enum_match(game.split()[1])
-    total_score += game_point(me, opponent)
+    total_score_part1 += game_point_move(my_move=me, opponents_move=opponent)
+    total_score_part2 += game_point_result(opponents_move=opponent, result=result_match(game.split()[1]))
 
-print(total_score)
+print(f"Day 02, Part 1: {total_score_part1}")
+print(f"Day 02, Part 2: {total_score_part2}")
