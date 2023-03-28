0cc4ea330d68baeb9336e09d5fde1d9eb78e0282
velibkolay <veliberkaykolay@gmail.com>
2023-03-29 01:59:04 +0200

    day05 part 1

--- days/day05.py
+++ days/day05.py
@@ -1,8 +1,7 @@
 import string
 import re
 
-input_day05 = open("../inputs/input_day05.txt").read()
-
+input_day05 = open("inputs/input_day05.txt").read()
 input_lines = input_day05.split("\n")
 crane = []
 
@@ -14,7 +13,7 @@ for line_index, line in enumerate(input_lines[:8]):
         if line[i] in string.ascii_uppercase:
             crane[line_index].update({i: line[i]})
 
-crates = []
+crates = list()
 for i in range(9):
     crates.append([])
 
@@ -22,7 +21,13 @@ for line in crane:
     for k, v in line.items():
         crates[k // 4].append(v)
 
+for line in input_lines[10:]:
+    count = int(re.search("move (.*) from", line).group(1))
+    from_crate = int(re.search("from (.*) to", line).group(1)) - 1
+    to_crate = int(re.search("to (.*)", line).group(1)) - 1
+    for _ in range(count):
+        crates[to_crate].insert(0, crates[from_crate].pop(0))
 
-print(crates)
-print(input_lines[10])
-print(re.search("from (.*) to", input_lines[10]).group(1))
+        
+for crate in crates:
+    print(crate[0], end="")
